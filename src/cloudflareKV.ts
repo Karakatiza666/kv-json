6bcd133fbbeb53178da7a064555f5fe319952d9b
George <bulakh.96@gmail.com>
2023-07-28 19:30:37 +0300

    Improve API, add migrations

--- src/cloudflareKV.ts
+++ src/cloudflareKV.ts
@@ -1,7 +1,5 @@
 import type { KVJson } from "src/kv"
 import { BSON } from 'bsonfy'
-const encoder = new TextEncoder()
-const decoder = new TextDecoder()
 
 export const jsonFromKVNamespace = (o: KVNamespace): KVJson => ({
    get<T>(key: string) { return o.get(key, 'json').then(b => b as T | null) },
@@ -11,16 +9,18 @@ export const jsonFromKVNamespace = (o: KVNamespace): KVJson => ({
       })
    },
    put<T>(key: string, value: T) { return o.put(key, JSON.stringify(value)) },
-   getRaw(key) {
-      return o.get(key, 'arrayBuffer').then(b => b ? new Uint8Array(b) : b)
-   },
-   reqRaw(key) {
-      return o.get(key, 'arrayBuffer').then(b => {
-         if (b === null) { throw new Error('KV: ' + key + ' not found!') }
-         return new Uint8Array(b)
-      })
-   },
-   putRaw(key, value) { return o.put(key, value) },
+   raw: {
+      get(key) {
+         return o.get(key, 'arrayBuffer').then(b => b ? new Uint8Array(b) : b)
+      },
+      req(key) {
+         return o.get(key, 'arrayBuffer').then(b => {
+            if (b === null) { throw new Error('KV: ' + key + ' not found!') }
+            return new Uint8Array(b)
+         })
+      },
+      put(key, value) { return o.put(key, value) },
+   }
 })
 
 export const jsonFromR2Bucket = (o: R2Bucket): KVJson => ({
@@ -31,16 +31,18 @@ export const jsonFromR2Bucket = (o: R2Bucket): KVJson => ({
       })
    },
    async put<T>(key: string, value: T) { await o.put(key, JSON.stringify(value)) },
-   getRaw(key) {
-      return o.get(key).then(async b => b ? new Uint8Array(await b.arrayBuffer()) : b)
-   },
-   reqRaw(key) {
-      return o.get(key).then(async b => {
-         if (b === null) { throw new Error('KV: ' + key + ' not found!') }
-         return new Uint8Array(await b.arrayBuffer())
-      })
-   },
-   async putRaw(key, value) { await o.put(key, value) },
+   raw: {
+      get(key) {
+         return o.get(key).then(async b => b ? new Uint8Array(await b.arrayBuffer()) : b)
+      },
+      req(key) {
+         return o.get(key).then(async b => {
+            if (b === null) { throw new Error('KV: ' + key + ' not found!') }
+            return new Uint8Array(await b.arrayBuffer())
+         })
+      },
+      async put(key, value) { await o.put(key, value) },
+   }
 })
 
 // =================
@@ -53,16 +55,18 @@ export const bsonFromKVNamespace = (o: KVNamespace): KVJson => ({
       })
    },
    put<T>(key: string, value: T) { return o.put(key, BSON.serialize(value)) },
-   getRaw(key) {
-      return o.get(key, 'arrayBuffer').then(b => b ? new Uint8Array(b) : b)
-   },
-   reqRaw(key) {
-      return o.get(key, 'arrayBuffer').then(b => {
-         if (b === null) { throw new Error('KV: ' + key + ' not found!') }
-         return new Uint8Array(b)
-      })
-   },
-   putRaw(key, value) { return o.put(key, value) },
+   raw: {
+      get(key) {
+         return o.get(key, 'arrayBuffer').then(b => b ? new Uint8Array(b) : b)
+      },
+      req(key) {
+         return o.get(key, 'arrayBuffer').then(b => {
+            if (b === null) { throw new Error('KV: ' + key + ' not found!') }
+            return new Uint8Array(b)
+         })
+      },
+      put(key, value) { return o.put(key, value) },
+   }
 })
 
 export const bsonFromR2Bucket = (o: R2Bucket): KVJson => ({
@@ -73,16 +77,18 @@ export const bsonFromR2Bucket = (o: R2Bucket): KVJson => ({
       })
    },
    async put<T>(key: string, value: T) { await o.put(key, BSON.serialize(value)) },
-   getRaw(key) {
-      return o.get(key).then(async b => b ? new Uint8Array(await b.arrayBuffer()) : b)
-   },
-   reqRaw(key) {
-      return o.get(key).then(async b => {
-         if (b === null) { throw new Error('KV: ' + key + ' not found!') }
-         return new Uint8Array(await b.arrayBuffer())
-      })
-   },
-   async putRaw(key, value) { await o.put(key, value) },
+   raw: {
+      get(key) {
+         return o.get(key).then(async b => b ? new Uint8Array(await b.arrayBuffer()) : b)
+      },
+      req(key) {
+         return o.get(key).then(async b => {
+            if (b === null) { throw new Error('KV: ' + key + ' not found!') }
+            return new Uint8Array(await b.arrayBuffer())
+         })
+      },
+      async put(key, value) { await o.put(key, value) },
+   }
 })
 
 // =================
@@ -95,34 +101,41 @@ export const ordBsonFromKVNamespace = (o: KVNamespace): KVJson => ({
       })
    },
    put<T>(key: string, value: T) { return o.put(key, BSON.serializeOrdered(value)) },
-   getRaw(key) {
-      return o.get(key, 'arrayBuffer').then(b => b ? new Uint8Array(b) : b)
-   },
-   reqRaw(key) {
-      return o.get(key, 'arrayBuffer').then(b => {
-         if (b === null) { throw new Error('KV: ' + key + ' not found!') }
-         return new Uint8Array(b)
-      })
-   },
-   putRaw(key, value) { return o.put(key, value) },
+   raw: {
+      get(key) {
+         return o.get(key, 'arrayBuffer').then(b => b ? new Uint8Array(b) : b)
+      },
+      req(key) {
+         return o.get(key, 'arrayBuffer').then(b => {
+            if (b === null) { throw new Error('KV: ' + key + ' not found!') }
+            return new Uint8Array(b)
+         })
+      },
+      put(key, value) { return o.put(key, value) },
+   }
 })
 
 export const ordBsonFromR2Bucket = (o: R2Bucket): KVJson => ({
-   get<T>(key: string) { return o.get(key).then(async r => r ? (await BSON.deserialize(new Uint8Array(await r.arrayBuffer()))) as T : null) },
-   req<T>(key: string) { return o.get(key).then(async r => {
+   get<T>(key: string) {
+      return o.get(key).then(async r => r ? (await BSON.deserialize(new Uint8Array(await r.arrayBuffer()))) as T : null)
+   },
+   req<T>(key: string) {
+      return o.get(key).then(async r => {
          if (!r) throw new Error('KV: ' + key + ' not found!')
          return BSON.deserialize(new Uint8Array(await r.arrayBuffer())) as T
       })
    },
    async put<T>(key: string, value: T) { await o.put(key, BSON.serializeOrdered(value)) },
-   getRaw(key) {
-      return o.get(key).then(async b => b ? new Uint8Array(await b.arrayBuffer()) : b)
-   },
-   reqRaw(key) {
-      return o.get(key).then(async b => {
-         if (b === null) { throw new Error('KV: ' + key + ' not found!') }
-         return new Uint8Array(await b.arrayBuffer())
-      })
-   },
-   async putRaw(key, value) { await o.put(key, value) },
+   raw: {
+      get(key) {
+         return o.get(key).then(async b => b ? new Uint8Array(await b.arrayBuffer()) : b)
+      },
+      req(key) {
+         return o.get(key).then(async b => {
+            if (b === null) { throw new Error('KV: ' + key + ' not found!') }
+            return new Uint8Array(await b.arrayBuffer())
+         })
+      },
+      async put(key, value) { await o.put(key, value) },
+   }
 })
--- src/kv.ts
+++ src/kv.ts
@@ -1,31 +1,42 @@
+// type ZZ = {
+//    get: <T>(key: string) => Promise<T | null>
+//    req: <T>(key: string) => Promise<T>
+//    put: <T>(key: string, value: T) => Promise<void>
+// }
 
-export type KVJson = {
+// type KVApi<X = unknown> = X extends Uint8Array ? {
+//    get: (key: string) => Promise<Uint8Array | null>
+//    req: (key: string) => Promise<Uint8Array>
+//    put: (key: string, value: Uint8Array) => Promise<void>
+// } : {
+//    get: <T>(key: string) => Promise<T | null>
+//    req: <T>(key: string) => Promise<T>
+//    put: <T>(key: string, value: T) => Promise<void>
+// }
+
+export type KVJson = // KVApi & { raw: KVApi<Uint8Array> }
+{
    get: <T>(key: string) => Promise<T | null>
    req: <T>(key: string) => Promise<T>
    put: <T>(key: string, value: T) => Promise<void>
-   getRaw: (key: string) => Promise<Uint8Array | null>
-   reqRaw: (key: string) => Promise<Uint8Array>
-   putRaw: (key: string, value: Uint8Array) => Promise<void>
+   raw: {
+      get: (key: string) => Promise<Uint8Array | null>
+      req: (key: string) => Promise<Uint8Array>
+      put: (key: string, value: Uint8Array) => Promise<void>
+   }
 }
 
 export type StaticKey<Model, Field extends string> = {
    get:     (ctx: {[_ in Field]: KVJson}) => Promise<Model | null>
    req:     (ctx: {[_ in Field]: KVJson}) => Promise<Model>
    put:     (ctx: {[_ in Field]: KVJson}, value: Model) => Promise<void>
-   getRaw:  (ctx: {[_ in Field]: KVJson}) => Promise<Uint8Array | null>
-   reqRaw:  (ctx: {[_ in Field]: KVJson}) => Promise<Uint8Array>
-   putRaw:  (ctx: {[_ in Field]: KVJson}, raw: Uint8Array) => Promise<void>
-   // fromRaw: (ctx: {[_ in Field]: KVJson}, raw: Uint8Array) => Promise<void>,
-}
+} & (Model extends Uint8Array ? {} : { raw: StaticKey<Uint8Array, Field> })
 
 export type DynamicKey<Model, V, Field extends string> = {
    get:     (ctx: {[_ in Field]: KVJson}, key: V) => Promise<Model | null>
    req:     (ctx: {[_ in Field]: KVJson}, key: V) => Promise<Model>
    put:     (ctx: {[_ in Field]: KVJson}, key: V, value: Model) => Promise<void>
-   getRaw:  (ctx: {[_ in Field]: KVJson}, key: V) => Promise<Uint8Array | null>
-   reqRaw:  (ctx: {[_ in Field]: KVJson}, key: V) => Promise<Uint8Array>
-   putRaw:  (ctx: {[_ in Field]: KVJson}, key: V, raw: Uint8Array) => Promise<void>
-}
+} & (Model extends Uint8Array ? {} : { raw: DynamicKey<Uint8Array, V, Field> })
 
 // const kvAtom = <Model>() => <K>(key: K, ..._: 0[]) => <TT extends 'fuzzy' | 'strict'>(type: TT):
 //    K extends string ? {
@@ -35,22 +46,76 @@ export type DynamicKey<Model, V, Field extends string> = {
 //    } => ({
 //    })
 
-export const kvAtom = <Model>() => <K, V>(keyPath: K, fs?: (v: V) => string) => <Field extends string>(type: Field):
-   K extends string ? StaticKey<Model, Field> : DynamicKey<Model, V, Field> =>
-   typeof keyPath === 'string' ? ({
-      get   (ctx) { return ctx[type].get<Model>(keyPath) },
-      req   (ctx) { return ctx[type].req<Model>(keyPath) },
-      put   (ctx, value) { return ctx[type].put<Model>(keyPath, value) },
-      getRaw(ctx) { return ctx[type].getRaw(keyPath) },
-      reqRaw(ctx) { return ctx[type].reqRaw(keyPath) },
-      putRaw(ctx, value) { return ctx[type].putRaw(keyPath, value) }
-   } satisfies StaticKey<Model, Field>)
-   : Array.isArray(keyPath) && fs ? ({
-      get   (ctx, key) { return ctx[type].get<Model>(keyPath.join(fs(key))) },
-      req   (ctx, key) { return ctx[type].req<Model>(keyPath.join(fs(key))) },
-      put   (ctx, key, value) { return ctx[type].put<Model>(keyPath.join(fs(key)), value) },
-      getRaw(ctx, key) { return ctx[type].getRaw(keyPath.join(fs(key))) },
-      reqRaw(ctx, key) { return ctx[type].reqRaw(keyPath.join(fs(key))) },
-      putRaw(ctx, key, value) { return ctx[type].putRaw(keyPath.join(fs(key)), value) }
-   } satisfies DynamicKey<Model, V, Field>)
-   : (() => { throw new Error('')}) as any
+type Migrations = Record<number, (v: any) => any>
+
+export const kvAtom = <Model>() => <K, V>(keyPath: K, fs?: (v: V) => string) =>
+   <Field extends string>(type: Field, migrations?: Migrations):
+   K extends string ? StaticKey<Model, Field> : DynamicKey<Model, V, Field> => {
+   const version = checkMigrations(migrations)
+   if (version === null) {
+      throw new Error('Inconsistent migrations!')
+   }
+   const withVersion: (x: any) => any = version === 0 ? (x => x) : (x => ({ [version]: x }))
+   return typeof keyPath === 'string' ? ({
+         get   (ctx) { return ctx[type].get<Model>(keyPath).then(runMigrations(migrations)) },
+         req   (ctx) { return ctx[type].req<Model>(keyPath).then(runMigrations(migrations)) },
+         put   (ctx, value) { return ctx[type].put<Model>(keyPath, withVersion(value)) },
+         raw: {
+            get   (ctx) { return ctx[type].raw.get(keyPath) },
+            req   (ctx) { return ctx[type].raw.req(keyPath) },
+            put   (ctx, value) { return ctx[type].raw.put(keyPath, value) },
+         }
+      } satisfies StaticKey<Model, Field>)
+      : Array.isArray(keyPath) && fs ? ({
+         get   (ctx, key) { return ctx[type].get<Model>(keyPath.join(fs(key))).then(runMigrations(migrations)) },
+         req   (ctx, key) { return ctx[type].req<Model>(keyPath.join(fs(key))).then(runMigrations(migrations)) },
+         put   (ctx, key, value) { return ctx[type].put<Model>(keyPath.join(fs(key)), withVersion(value)) },
+         raw: {
+            get(ctx, key) { return ctx[type].raw.get(keyPath.join(fs(key))) },
+            req(ctx, key) { return ctx[type].raw.req(keyPath.join(fs(key))) },
+            put(ctx, key, value) { return ctx[type].raw.put(keyPath.join(fs(key)), value) }
+         }
+      } satisfies DynamicKey<Model, V, Field>)
+      : (() => { throw new Error('')}) as any
+}
+
+/**
+ * Check if migration ids are integer and do not have gaps
+ * @param migrations 
+ */
+const checkMigrations = (migrations?: Migrations) => {
+   if (!migrations) return 0
+   const keys = Object.keys(migrations)
+   const ids = keys.filter(/^\d+$/.test).map(parseInt).sort()
+   return keys.length === ids.length && ids.every((n, i) => n === ids[0] + i)
+      ? ids[ids.length - 1]
+      : null
+}
+
+const runMigrations = (migrations?: Migrations) => <T>(raw: T): T => {
+   if (!migrations) {
+      return raw
+   }
+   if (raw === null) {
+      return raw
+   }
+   // const ids = Object.keys(migrations).map(parseInt).sort()
+   // let id = +(!(o instanceof Array) && typeof o === 'object' && o !== null && (ks => ks.length === 1 && parseInt(ks[0]))(Object.keys(o)))
+   let [id, obj] = (() => {
+      if (raw instanceof Array)
+         return [0, raw]
+      if (typeof raw !== 'object' || raw === null)
+         return [0, raw]
+      const key = (ks => ks.length === 1 && parseInt(ks[0]))(Object.keys(raw))
+      if (!key && key !== 0)
+         return [0, raw]
+      return [key, (raw as any)[key]]
+   })()
+   let migrate: Migrations[number] | undefined
+   while (migrate = migrations[id]) {
+      obj = migrate(obj)
+      ++id
+   }
+   // return [id, obj]
+   return obj
+}
